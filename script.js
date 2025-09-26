492dbdf8a3a9f2b1d74775f4ee7dbbeab0c073fb
Jay Abhani <abhani57@gmail.com>
2025-09-26 12:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+// Select dropdown aur button ko pakdo
+const select = document.getElementById("colorSelect");
+const button = document.querySelector('input[type="button"]');
+
+// Button click event
+button.addEventListener("click", () => {
+  const selectedIndex = select.selectedIndex; // jo option select hai uska index
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex); // wahi option delete ho jayega
+  }
+});
